5e5fe18cbcc3a495a3bf9731e6936cb32492a079
Nenetti <akinori1090@gmail.com>
2018-11-10 18:33:09 +0900

    0.0

--- src/dictionary/Dictionary.java
+++ src/dictionary/Dictionary.java
@@ -50,6 +50,7 @@ public class Dictionary {
 	
 	public Session getSession(String key, Language language) {
 		Sessions sessions=getSessions(key, language);
+		System.out.println(sessions);
 		if(sessions!=null) {
 			return sessions.getSession(language);
 		}
